bba8273fc65d9fdb014e46af2f9509a97226ec5f
Ritu Jain <ritujain1009@gmail.com>
2025-09-14 22:47:21 -0500

    Two Sum Problem

---
+++ arrays/TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum
+{
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        Map<Integer, Integer> numsToIndexMap = new HashMap<>();
+        for ( int i=0;i<length;i++) {
+            int difference = taget - nums[i];
+            if(numsToIndexMap.containsKey(difference)) {
+                return new int[] {i, numsToIndexMap.get(difference)};
+            }
+            numsToIndexMap.put(nums[i],i);
+        }
+        return new int[] {-1, -1};
+    }
+}
